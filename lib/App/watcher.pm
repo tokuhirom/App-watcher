73adb7724cd1e4316f9b7c6fe3f4bcd4898d6a9f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-08-19 18:48:48 +0900

    initial import

---
+++ lib/App/watcher.pm
@@ -0,0 +1,39 @@
+package App::watcher;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::watcher -
+
+=head1 SYNOPSIS
+
+  use App::watcher;
+
+=head1 DESCRIPTION
+
+App::watcher is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
